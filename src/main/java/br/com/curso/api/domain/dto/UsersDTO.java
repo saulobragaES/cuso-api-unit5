c2e68070e86cc310663459e39221ef27b27f2873
saulobragaES <saulobraga_es@hotmail.com>
2022-07-02 20:43:53 -0300

    Dependencia para ModelMapper

---
+++ src/main/java/br/com/curso/api/domain/dto/UsersDTO.java
@@ -0,0 +1,17 @@
+package br.com.curso.api.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter @Getter
+@AllArgsConstructor  @NoArgsConstructor
+public class UsersDTO {
+
+    private  Integer id;
+    private  String name;
+    private  String email;
+    private  String password;
+
+}
