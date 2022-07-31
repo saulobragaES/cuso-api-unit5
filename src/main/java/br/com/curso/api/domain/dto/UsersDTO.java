87a05c157b66564c20ddbab13f292f545526524e
saulobragaES <saulobraga_es@hotmail.com>
2022-07-31 16:22:24 -0300

    Teste para manipulador de exceções metodo object not found

--- src/main/java/br/com/curso/api/domain/dto/UsersDTO.java
+++ src/main/java/br/com/curso/api/domain/dto/UsersDTO.java
@@ -8,7 +8,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter @Getter
-@AllArgsConstructor  @NoArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
 public class UsersDTO {
 
     private  Integer id;
